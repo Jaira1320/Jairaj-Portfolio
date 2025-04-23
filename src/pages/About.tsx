2523cd6221d580080664b52090aee67ad7a0fbf6
Jairaj1320 <mattajairajsingh@gmail.com>
2025-04-23 15:38:00 +0530

    added the image f

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -44,8 +44,8 @@ const About = () => {
             <div className="lg:col-span-1">
               <div className="sticky top-24">
                 <div className="relative mb-6 overflow-hidden rounded-xl">
-                  <img
-                    src="fuploads/da676bdd-37f1-43ce-a44b-9c34473287ec.png"
+                <img
+                    src="src/pages/PHOTO-2025-03-28-14-36-39.jpg"
                     alt="Jairaj Singh Matta"
                     className="w-full h-auto"
                   />
